d1a8dfff51250d43ea28b807f51419e209410bbb
Raushaniam <mullakhmetova18@gmail.com>
2022-08-24 17:42:24 +0400

    feat: implement button 'Pull map' and add styles

--- codejam-eldritch/index.js
+++ codejam-eldritch/index.js
@@ -2,6 +2,9 @@ const ancient = document.querySelector('.ancient');
 const level = document.querySelector('.level');
 const deckContainer = document.querySelector('.deck-container');
 const shuffleButton = document.querySelector('.shuffle-button');
+const cardDeck = document.querySelector('.card-deck');
+const deck = document.createElement('span');
+deck.innerHTML = 'Вытянуть карту';
 const btn = document.createElement('span');
 btn.innerHTML = 'Замешать колоду';
 
@@ -21,6 +24,8 @@ level.addEventListener('click', setBtn);
 const setDeck = () => {
     shuffleButton.firstChild.remove()
     deckContainer.classList.add('deck-container-active');
+    cardDeck.prepend(deck);
+    deck.classList.add('active');
 }
 
 btn.addEventListener('click', setDeck);
